ea462f534ec5f377024ada745d8a4431e5483c43
tyler <tylermontobbio@gmail.com>
2023-05-25 21:31:57 -0700

    added bin/rails db:create to render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,5 +9,6 @@ cp -a client/build/. public/
 
 # Build commands for back end
 bundle install
+bin/rails db:create
 bundle exec rake db:migrate 
 bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only to avoid duplicate records
